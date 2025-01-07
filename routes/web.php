a46affff5cd80423ae37c6b7c55f9455d77c7ffe
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-07 15:52:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\KoleksiBukuController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('koleksi-buku.index');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'loginPost']);
 });
+
+
+Route::get('/', [KoleksiBukuController::class, 'index']);
+
+Route::get('/signup', function () {
+    return view('signup.index');
+});
+
+
