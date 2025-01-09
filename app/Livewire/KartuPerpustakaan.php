afd476cda45bdd57da3b01ba6f39a08dcba06716
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-09 15:41:43 +0700

    Create livewire controller

---
+++ app/Livewire/KartuPerpustakaan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class KartuPerpustakaan extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontpage.kartu-perpustakaan');
+    }
+}
