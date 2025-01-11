be750f8b16439feae2365376b466bc4425b7788b
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-11 21:53:23 +0700

    create new livewire controller

---
+++ app/Livewire/Anggota.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Anggota extends Component
+{
+    public function render()
+    {
+        return view('livewire.adminpage.anggota');
+    }
+}
---
+++ app/Livewire/Rekapitulasi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Rekapitulasi extends Component
+{
+    public function render()
+    {
+        return view('livewire.adminpage.rekapitulasi');
+    }
+}
