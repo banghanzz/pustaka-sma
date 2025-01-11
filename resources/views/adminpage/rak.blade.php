0c844ff477f877495630cf682044ca35a0445fd2
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-11 21:52:59 +0700

    create new views

---
+++ resources/views/adminpage/rak.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.admin')
+
+@section('content')
+    {{-- Sidebar --}}
+    @include('components.sideNavbar')
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+            @include('components.topBarAdmin')
+            @livewire('rak')
+        </div>
+    </div>
+@endsection
