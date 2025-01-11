976d1ba2ac4ff76fb6087b0be6817f8876505d84
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-11 21:53:51 +0700

    create view function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,4 +12,46 @@ class DashboardController extends Controller
             'title' => 'Dashboard',
         ]);
     }
+    public function transaksi()
+    {
+        return view('adminpage.transaksi',[
+            'title' => 'Transaksi Peminjaman',
+        ]);
+    }
+    public function rak()
+    {
+        return view('adminpage.rak',[
+            'title' => 'Rak Buku',
+        ]);
+    }
+    public function kategori()
+    {
+        return view('adminpage.kategori',[
+            'title' => 'Kategori Buku',
+        ]);
+    }
+    public function koleksibuku()
+    {
+        return view('adminpage.koleksibuku',[
+            'title' => 'Koleksi Buku',
+        ]);
+    }
+    public function bukurusak()
+    {
+        return view('adminpage.bukurusak',[
+            'title' => 'Buku Rusak',
+        ]);
+    }
+    public function rekapitulasi()
+    {
+        return view('adminpage.rekapitulasi',[
+            'title' => 'Rekapitulasi',
+        ]);
+    }
+    public function anggota()
+    {
+        return view('adminpage.anggota',[
+            'title' => 'Anggota Perpustakaan',
+        ]);
+    }
 }
