f25c91b0cc34f62df589e919fb4796dfc69f0fd4
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-11 22:00:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::group(['middleware' => 'guest'], function () {
 });
 
 Route::get('/signup', function () {
-    return view('signup.index');
+    return view('signup.index', [
+        'title' => 'Daftar Akun',
+    ]);
 });
 
 Route::get('/', [KoleksiBukuController::class, 'index']);
