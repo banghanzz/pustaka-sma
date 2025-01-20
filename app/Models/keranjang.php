f1b7e355de24aa335317598f91e8091c7eff4cc6
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-20 15:42:50 +0700

    rename

--- app/Models/keranjang.php
+++ app/Models/keranjang.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class keranjang extends Model
+class Keranjang extends Model
 {
     use HasFactory;
 
