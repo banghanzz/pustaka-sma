dc7c66b1088b30f2d30d75edac52e4c83757c651
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-20 14:30:06 +0700

    Create models

---
+++ app/Models/buku_rusak.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class buku_rusak extends Model
+{
+    use HasFactory;
+
+    protected $table = 'buku_rusak';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+    public $incrementing = false;
+
+    public function buku()
+    {
+        return $this->belongsTo(Buku::class, 'buku_id');
+    }
+}
