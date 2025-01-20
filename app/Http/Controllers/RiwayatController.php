c01da9aae09c2039ff5c5264cbf50accf8a69637
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-20 14:30:35 +0700

    Create view controller

--- app/Http/Controllers/RiwayatController.php
+++ app/Http/Controllers/RiwayatController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\DetailPeminjaman;
+use Illuminate\Support\Facades\Auth;
 
 class RiwayatController extends Controller
 {
     public function index()
     {
-        return view('frontpage.riwayat',[
+        $userId = Auth::id();
+
+        $riwayatPeminjaman = DetailPeminjaman::whereIn('status_peminjaman', ['selesai', 'dibatalkan'])
+            ->whereHas('keranjang', function ($query) use ($userId) {
+                $query->where('users_id', $userId);
+            })
+            ->get();
+
+        return view('frontpage.riwayat', [
             'title' => 'Riwayat',
+            'riwayatPeminjaman' => $riwayatPeminjaman,
         ]);
     }
 }
