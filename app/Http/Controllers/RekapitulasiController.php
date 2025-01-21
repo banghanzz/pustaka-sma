8eee1215433ce56f1ec19563c335347f67c5195b
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-21 21:28:27 +0700

    add view function for rekapitulasi frontpage

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BukuRusak as ModelsBukuRusak;
+use App\Models\Buku;
 
 class RekapitulasiController extends Controller
 {
     public function index()
     {
+        $rekapitulasi = ModelsBukuRusak::with('buku')->orderBy(Buku::select('judul')->whereColumn('buku_id', 'id'), 'asc')->get();
+        $totalJumlah = $rekapitulasi->sum(function($bukuRusak) {
+            return $bukuRusak->buku->stok;
+        });
+
         return view('frontpage.rekapitulasi',[
             'title' => 'Rekapitulasi',
+            'rekapitulasi' => $rekapitulasi,
+            'totalJumlah' => $totalJumlah,
+            'totalRusakRingan' => $rekapitulasi->sum('rusak_ringan'),
+            'totalRusakSedang' => $rekapitulasi->sum('rusak_sedang'),
+            'totalRusakBerat' => $rekapitulasi->sum('rusak_berat'),
+            'totalRusak' => $rekapitulasi->sum('rusak_ringan') + $rekapitulasi->sum('rusak_sedang') + $rekapitulasi->sum('rusak_berat'),
         ]);
     }
 }
--- resources/views/frontpage/rekapitulasi.blade.php
+++ resources/views/frontpage/rekapitulasi.blade.php
@@ -10,49 +10,44 @@
 
         {{-- Table --}}
         <div wire:ignore class="py-4 mb-3">
-            <table class="table text-center table-bordered table-striped" id="rekapTable">
+            <table class="table text-center table-bordered table-striped" id="">
                 <thead class="table-dark">
                     <tr>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="1%">#</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Judul Buku</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Penulis</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Kategori</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Rusak Ringan</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Rusak Sedang</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Rusak Berat</th>
-                        <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Jumlah</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="1%">#</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Judul Buku</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Penulis</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Kategori</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Jumlah Buku</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Rusak Ringan</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Rusak Sedang</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Rusak Berat</th>
+                        <th class="fs-6 fw-semibold text-center align-middle" width="">Total Rusak</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {{-- @foreach ($slots as $slot) --}}
+                    @foreach ($rekapitulasi as $itemRekapitulasi)   
                         <tr>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
-                            <td class="align-middle fs-6"></td>
+                            <td class="align-middle">{{ $loop->iteration }}</td>
+                            <td class="align-middle">{{ $itemRekapitulasi->buku->judul }}</td>
+                            <td class="align-middle">{{ $itemRekapitulasi->buku->penulis }}</td>
+                            <td class="align-middle">{{ $itemRekapitulasi->buku->kategori->nama }}</td>
+                            <td class="align-middle text-center">{{ $itemRekapitulasi->buku->stok }}</td>
+                            <td class="align-middle text-center">{{ $itemRekapitulasi->rusak_ringan }}</td>
+                            <td class="align-middle text-center">{{ $itemRekapitulasi->rusak_sedang }}</td>
+                            <td class="align-middle text-center">{{ $itemRekapitulasi->rusak_berat }}</td>
+                            <td class="align-middle text-center">{{ $itemRekapitulasi->rusak_ringan + $itemRekapitulasi->rusak_sedang + $itemRekapitulasi->rusak_berat }}</td>
                         </tr>
-                    {{-- @endforeach --}}
+                    @endforeach
                     <tr>
-                        <td class="align-middle fs-6" colspan="4">Jumlah Total</td>
-                        <td class="align-middle fs-6"></td>
-                        <td class="align-middle fs-6"></td>
-                        <td class="align-middle fs-6"></td>
-                        <td class="align-middle fs-6"></td>
+                        <td colspan="4"><strong>Total</strong></td>
+                        <td class="fw-semibold">{{ $totalJumlah }}</td>
+                        <td class="fw-semibold">{{ $totalRusakRingan }}</td>
+                        <td class="fw-semibold">{{ $totalRusakSedang }}</td>
+                        <td class="fw-semibold">{{ $totalRusakBerat }}</td>
+                        <td class="fw-semibold">{{ $totalRusak }}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
-@endsection
-
-@section('script')
-<script>
-    $(document).ready( function () {
-    $('#rekapTable').DataTable();
-} );
-</script>
 @endsection
