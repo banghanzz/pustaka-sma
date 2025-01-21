65fab64f932db2bc28cce11cab2a4b9cd38c9570
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-01-21 22:42:46 +0700

    Add Download PDF

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 use App\Models\BukuRusak as ModelsBukuRusak;
 use App\Models\Buku;
@@ -24,5 +25,32 @@ class RekapitulasiController extends Controller
             'totalRusakBerat' => $rekapitulasi->sum('rusak_berat'),
             'totalRusak' => $rekapitulasi->sum('rusak_ringan') + $rekapitulasi->sum('rusak_sedang') + $rekapitulasi->sum('rusak_berat'),
         ]);
+    }
+
+    public function downloadPDF()
+    {
+        $rekapitulasi = ModelsBukuRusak::with('buku')->orderBy(Buku::select('judul')->whereColumn('buku_id', 'id'), 'asc')->get();
+        $totalJumlah = $rekapitulasi->sum(function($bukuRusak) {
+            return $bukuRusak->buku->stok;
+        });
+
+        $data = [
+            'title' => 'Rekapitulasi Data',
+            'date' => date('m/d/Y'),
+            'rekapitulasi' => $rekapitulasi,
+            'totalJumlah' => $totalJumlah,
+            'totalRusakRingan' => $rekapitulasi->sum('rusak_ringan'),
+            'totalRusakSedang' => $rekapitulasi->sum('rusak_sedang'),
+            'totalRusakBerat' => $rekapitulasi->sum('rusak_berat'),
+            'totalRusak' => $rekapitulasi->sum('rusak_ringan') + $rekapitulasi->sum('rusak_sedang') + $rekapitulasi->sum('rusak_berat'),
+            'currentMonth' => date('F'),
+            'currentYear' => date('Y'),
+        ];
+
+        $pdf = app('dompdf.wrapper');
+        $pdf->setPaper('A4', 'portrait');
+        $pdf->loadView('pdf.rekapitulasi', $data);
+
+        return $pdf->stream('rekapitulasi.pdf');
     }
 }
