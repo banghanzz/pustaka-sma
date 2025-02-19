782876b8bf18790d21d083fc224e4cbcd3d06b6b
Farhan Al Rasyid <100454264+banghanzz@users.noreply.github.com>
2025-02-19 23:09:18 +0700

    Update keranjang.blade.php

--- resources/views/frontpage/keranjang.blade.php
+++ resources/views/frontpage/keranjang.blade.php
@@ -34,12 +34,16 @@
                         <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Penulis</th>
                         <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Jumlah</th>
                         <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Lokasi</th>
-                        @if ($detailPeminjaman->filter(function($item) {
-                            return in_array($item->status_peminjaman, ['menunggu', 'dipinjam', 'dibatalkan', 'terlambat']);
-                            })->isNotEmpty())
-                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Nomor Pinjaman</th>
-                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Tanggal Pinjam</th>
-                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Tanggal Kembali</th>
+                        @if (
+                            $detailPeminjaman->filter(function ($item) {
+                                    return in_array($item->status_peminjaman, ['menunggu', 'dipinjam', 'dibatalkan', 'terlambat']);
+                                })->isNotEmpty())
+                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Nomor
+                                Pinjaman</th>
+                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Tanggal
+                                Pinjam</th>
+                            <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Tanggal
+                                Kembali</th>
                             <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Denda</th>
                             <th class="fs-6 fw-semibold text-center align-middle" scope="col" width="">Status</th>
                         @endif
@@ -50,69 +54,81 @@
                 </thead>
                 <tbody>
                     @forelse ($detailPeminjaman as $itemPeminjaman)
-                    <tr>
-                        <td class="align-middle fs-6">{{ $loop->iteration }}</td>
-                        <td class="align-middle text-center">
-                            <img src="{{ $itemPeminjaman->buku->sampul ? Storage::url($itemPeminjaman->buku->sampul) : asset('assets/images/default_cover_book.jpg') }}" 
-                                onerror="this.onerror=null;this.src='{{ asset('assets/images/default_cover_book.jpg') }}';" 
-                                class="img-fluid rounded shadow" width="60px">
-                        </td>
-                        <td class="align-middle fs-6">{{ $itemPeminjaman->buku->judul ?? 'Data Buku tidak ditemukan' }}</td>
-                        <td class="align-middle fs-6">{{ $itemPeminjaman->buku->penulis ?? 'Data Buku tidak ditemukan' }}</td>
-                        <td class="align-middle text-center fs-6">{{ $itemPeminjaman->jumlah }}</td>
-                        <td class="align-middle fs-6">Rak {{ $itemPeminjaman->buku->rak->rak ?? 'Data rak tidak ditemukan' }} - Baris {{ $itemPeminjaman->buku->rak->baris ?? '' }}</td>
-                        @if (in_array($itemPeminjaman->status_peminjaman, ['menunggu', 'dipinjam', 'dibatalkan', 'terlambat']))
-                            <td class="align-middle fs-6">{{ $itemPeminjaman->nomor_pinjaman }}</td>
-                            <td class="align-middle text-center fs-6">{{ date('d-m-Y', strtotime($itemPeminjaman->tanggal_pinjam)) }}</td>
-                            <td class="align-middle text-center fs-6">{{ date('d-m-Y', strtotime($itemPeminjaman->tanggal_kembali)) }}</td>                            <td class="align-middle fs-6">{{ $itemPeminjaman->denda ?? '-' }}</td>
+                        <tr>
+                            <td class="align-middle fs-6">{{ $loop->iteration }}</td>
+                            <td class="align-middle text-center">
+                                <img src="{{ $itemPeminjaman->buku->sampul ? Storage::url($itemPeminjaman->buku->sampul) : asset('assets/images/default_cover_book.jpg') }}"
+                                    onerror="this.onerror=null;this.src='{{ asset('assets/images/default_cover_book.jpg') }}';"
+                                    class="img-fluid rounded shadow" width="60px">
+                            </td>
+                            <td class="align-middle fs-6">{{ $itemPeminjaman->buku->judul ?? 'Data Buku tidak ditemukan' }}
+                            </td>
                             <td class="align-middle fs-6">
-                                
+                                {{ $itemPeminjaman->buku->penulis ?? 'Data Buku tidak ditemukan' }}</td>
+                            <td class="align-middle text-center fs-6">{{ $itemPeminjaman->jumlah }}</td>
+                            <td class="align-middle fs-6">Rak
+                                {{ $itemPeminjaman->buku->rak->rak ?? 'Data rak tidak ditemukan' }} - Baris
+                                {{ $itemPeminjaman->buku->rak->baris ?? '' }}</td>
+                            @if (in_array($itemPeminjaman->status_peminjaman, ['menunggu', 'dipinjam', 'dibatalkan', 'terlambat']))
+                                <td class="align-middle fs-6">{{ $itemPeminjaman->nomor_pinjaman }}</td>
+                                <td class="align-middle text-center fs-6">
+                                    {{ date('d-m-Y', strtotime($itemPeminjaman->tanggal_pinjam)) }}</td>
+                                <td class="align-middle text-center fs-6">
+                                    {{ date('d-m-Y', strtotime($itemPeminjaman->tanggal_kembali)) }}</td>
+                                <td class="align-middle fs-6">{{ $itemPeminjaman->denda ?? '-' }}</td>
+                                <td class="align-middle fs-6">
+
                                     @switch($itemPeminjaman->status_peminjaman)
                                         @case('menunggu')
                                             <div class="alert alert-warning text-center m-0" role="alert">
                                                 Menunggu persetujuan Pustakawan
                                             </div>
-                                            @break
+                                        @break
+
                                         @case('dibatalkan')
                                             <div class="alert alert-warning text-center m-0" role="alert">
                                                 Peminjaman kamu tidak disetujui Pustakawan
                                             </div>
-                                            @break
+                                        @break
+
                                         @case('dipinjam')
                                             <div class="alert alert-info text-center m-0" role="alert">
                                                 Kamu sedang meminjam
                                             </div>
-                                            @break
+                                        @break
+
                                         @case('terlambat')
                                             <div class="alert alert-danger text-center m-0" role="alert">
                                                 Segera kembalikan buku, denda akan bertambah setiap harinya
                                             </div>
-                                            @break
+                                        @break
+
                                         @default
                                             {{ ucfirst($itemPeminjaman->status_peminjaman) }}
                                     @endswitch
-                                </div>
-                            </td>
-                        @endif
-                        @if ($itemPeminjaman->status_peminjaman == 'keranjang')    
-                        <td class="align-middle fs-6">
-                            <form action="{{ route('keranjang.remove', $itemPeminjaman->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-outline-danger btn-sm"><i class="bi bi-trash3"></i></button>
-                            </form>
-                        </td>
-                        @endif
-                    </tr>
-                @empty
-                @endforelse
-                </tbody>
+        </div>
+        </td>
+        @endif
+        @if ($itemPeminjaman->status_peminjaman == 'keranjang')
+            <td class="align-middle fs-6">
+                <form action="{{ route('keranjang.remove', $itemPeminjaman->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-outline-danger btn-sm"><i class="bi bi-trash3"></i></button>
+                </form>
+            </td>
+        @endif
+        </tr>
+        @empty
+            @endforelse
+            </tbody>
             </table>
         </div>
 
-        @if ($detailPeminjaman->filter(function($item) {
-            return in_array($item->status_peminjaman, ['keranjang']);
-            })->isNotEmpty())
+        @if (
+            $detailPeminjaman->filter(function ($item) {
+                    return in_array($item->status_peminjaman, ['keranjang']);
+                })->isNotEmpty())
             {{-- Button Ajukan Pinjaman --}}
             <div class="row justify-content-center mb-5">
                 <div class="col-md-4">
@@ -120,20 +136,39 @@
                         @csrf
                         <div class="input-group-lg mb-3">
                             <label for="tanggalpinjam" class="form-label">Tanggal Mulai Pinjam</label>
-                            <input type="date" class="form-control" id="tanggalpinjam" name="tanggal_pinjam" value="{{ date('Y-m-d') }}" required>
+                            <input type="date" class="form-control" id="tanggalpinjam" name="tanggal_pinjam"
+                                value="{{ date('Y-m-d') }}" required>
                         </div>
-                        <button type="submit" class="btn btn-primary px-5 py-2 fs-5 fw-semibold w-100">Ajukan Peminjaman</button>
+                        <div class="input-group-lg mb-3">
+                            <label for="tanggalkembali" class="form-label">Tanggal Kembali</label>
+                            <input type="date" class="form-control" id="tanggalkembali" name="tanggal_kembali"
+                                value="{{ date('Y-m-d', strtotime('+3 days')) }}" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary px-5 py-2 fs-5 fw-semibold w-100">Ajukan
+                            Peminjaman</button>
                     </form>
                 </div>
             </div>
         @endif
-    </div>
-@endsection
+        </div>
+    @endsection
 
-@section('script')
-    <script>
-        $(document).ready(function() {
-            $('#keranjangTable').DataTable();
-        });
-    </script>
-@endsection
+    @section('script')
+        <script>
+            $(document).ready(function() {
+                $('#keranjangTable').DataTable();
+
+                // Fungsi untuk mengatur tanggal kembali minimal
+                function updateTanggalKembali() {
+                    const tanggalPinjam = $('#tanggalpinjam').val();
+                    $('#tanggalkembali').attr('min', tanggalPinjam);
+                }
+
+                // Panggil fungsi saat halaman dimuat
+                updateTanggalKembali();
+
+                // Update minimal tanggal kembali saat tanggal pinjam berubah
+                $('#tanggalpinjam').on('change', updateTanggalKembali);
+            });
+        </script>
+    @endsection
